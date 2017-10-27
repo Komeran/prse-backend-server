35825c533bd340ea6522f6e1bc6488b5d5b8eef3
Komeran <komerankesum@gmail.com>
2017-10-27 13:56:54 +0200

    Added first functionality and package.json

--- prse-server.js
+++ prse-server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+var config = require('./prse-server-config.json');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/ping', function (req, res) {
+  res.send('pong');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/crafting', function(req, res) {
+	res.send('Incomplete Request');
+});
+
+app.get('/crafting/recipe/:item', function(req, res) {
+	var item = req.params.item;
+	res.send('Requested recipe for: '+item);
+});
+
+app.post('/crafting/item/add', function(req, res){
+	res.send(200);
+});
+
+app.listen(config.port, function () {
+  console.log('PRSE Backend Server listening on port '+config.port+'!');
 });
